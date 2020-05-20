a1c60595dcf9833e3227839488be381d73b92c3d
Biswajeet Gope <biswajeetg@geekyants.com>
2020-05-20 16:33:04 +0530

    added icons in card

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import './style.scss';
+import "./style.scss";
 
 export const CardWrapper = (props) => {
   return (
     <Card className="mb-4">
-      <Card.Header className={props.footerContent?"header":""}>{props.content}</Card.Header>
-      <Card.Body>
-        {props.children}
-      </Card.Body>
-      {props.footerContent && (<Card.Footer className="footer">
-        {props.footerContent}
-      </Card.Footer>)}
+      <Card.Header className={props.footerContent ? "header" : ""}>
+        {props.icon}
+        {props.content}
+      </Card.Header>
+      <Card.Body>{props.children}</Card.Body>
+      {props.footerContent && (
+        <Card.Footer className="footer">{props.footerContent}</Card.Footer>
+      )}
     </Card>
   );
 };
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartBar,
+  faChartArea
+} from "@fortawesome/free-solid-svg-icons";
+
 import { CustomBreadCrum } from "../../components/breadcrum";
 import { Container, Row, Col } from "react-bootstrap";
 import { CustomCard } from "../../components/card";
@@ -6,9 +12,14 @@ import { AreaChart } from "../../components/areachart";
 import { BarChart } from "../../components/barchart";
 import { CustomTable } from "../../components/table";
 
-import "./style.scss";
+
 import { cardData } from "./data";
 import { CardWrapper } from "../../components/wrapper";
+import { TableSvg } from '../dashboard/icon/table';
+import "./style.scss";
+
+const barChartIcon = (<FontAwesomeIcon icon={faChartBar} />);
+const areaChartIcon =(<FontAwesomeIcon icon={faChartArea} />)
 
 export const Dashboard = () => {
   return (
@@ -31,18 +42,18 @@ export const Dashboard = () => {
         </Row>
         <Row>
           <Col>
-            <CardWrapper content="Area Chart Example">
+            <CardWrapper content="Area Chart Example" icon={areaChartIcon}>
               <AreaChart />
             </CardWrapper>
           </Col>
           <Col>
-            <CardWrapper content="Bar Chart Example">
+            <CardWrapper content="Bar Chart Example" icon={barChartIcon}>
               <BarChart />
             </CardWrapper>
           </Col>
         </Row>
 				<Row>
-					<CardWrapper content="DataTable Example">
+					<CardWrapper content="DataTable Example" icon={TableSvg}>
 					<CustomTable />
 					</CardWrapper>
         </Row>
